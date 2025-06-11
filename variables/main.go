6beec6d8d13ee2899dd64cfb5aa1cb65a7892fe1
Aniket Mahajan <mahajananiket207@gmail.com>
2025-06-11 09:33:21 +0530

    Learn about short variables

--- variables/main.go
+++ variables/main.go
@@ -6,7 +6,14 @@ func main() {
 	var s string
 	var n int
 	var b bool
+	var i, j, k int // multiple variable declaration of same type
+	var bo, flo, st = true, 3.14, "hello" // multiple variable declaration of different types
 	fmt.Println(s) // " " it will print empty string
 	fmt.Println(n) // 0
 	fmt.Println(b) // false
+
+	aniket := "Aniket" // short variable declaration
+	fmt.Println(aniket)
+
+	freq := rand.float64() * 3.0
 }
