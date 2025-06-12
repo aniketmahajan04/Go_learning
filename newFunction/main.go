5f08d9800fa5ddf945b4a891b8356d4c416ddc10
Aniket Mahajan <mahajananiket207@gmail.com>
2025-06-12 10:45:17 +0530

    Learn about lifetime of variables

--- newFunction/main.go
+++ newFunction/main.go
@@ -8,4 +8,21 @@ func main() {
 	fmt.Println(*p) // prints 0, the zero value of int
 	*p = 2
 	fmt.Println(*p) // prints 2, the value we set
+	a := newInt()
+	fmt.Println(a)
+	b := newInt2()
+	fmt.Println(b)
+
+	c := new(int)
+	d := new(int)
+	fmt.Println(c == d) // false, c and d are different pointers
+}
+
+func newInt() *int {
+	return new(int) // returns a pointer to a new int
+}
+
+func newInt2() *int {
+    var dummy int
+	return &dummy
 }
