212c2eb66e4a64b86b7cf9257266f9db4577befa
Aniket Mahajan <mahajananiket207@gmail.com>
2025-06-14 17:19:45 +0530

    Learn about packages and import

--- hello_world/main.go
+++ hello_world/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
-	"mylearning/tempconv"
+	"mylearning/tempconv" // package importing from tempconv
 )
 
 func main() {
 
-	fmt.Println("Hello, world!")
-	tempconv.PrintValue()
-	fmt.Println(tempconv.BoilingC)
+	// fmt.Println("Hello, world!")
+	// tempconv.PrintValue()
+	// fmt.Println(tempconv.BoilingC)
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+		   fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Farhenheit(t)
+		c := tempconv.Celsius(f)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
 }
