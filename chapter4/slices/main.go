9be5010a7ac7828a40de58040ac83ddc6e7c33e5
Aniket Mahajan <mahajananiket207@gmail.com>
2025-06-28 09:29:26 +0530

    custom append function

--- chapter4/slices/main.go
+++ chapter4/slices/main.go
@@ -23,13 +23,50 @@ func main() {
 	// 		}
 	// 	}
 	// }
-	a := [...]int{1, 2, 3, 4, 5}
-	reverse(a[:]) // a[:] converting array a into slice of same data
-	fmt.Println(a)
+	a := []int{1, 2, 3, 4, 5}
+	b := []int{1, 2, 3, 4, 5}
+	// reverse(a[:]) // a[:] converting array a into slice of same data
+	fmt.Println(equal(a, b))
+	// nil slices
+	// var w []int     // len(s) == 0, s == nil
+	// w = nil 		// len(s) == 0, s == nil
+	// w = []int(nil)  // len(s) == 0, s == nil
+	// w = []int{}     // len(s) == 0, s != nil
+}
+
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		// There is insufficient space. Allocate a new array.
+		// Grow by doubling, for amortized linear complexity.
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	z[len(x)] = y
+	return z
 }
 
 func reverse(a []int) {
 	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
 		a[i], a[j] = a[j], a[i]
 	}
+}
+
+func equal(x, y []int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
 }
