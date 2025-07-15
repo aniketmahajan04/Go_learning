0953a0515c5804db706397e99f367cfcf282136c
Aniket Mahajan <mahajananiket207@gmail.com>
2025-07-15 14:25:53 +0530

    variadic function

---
+++ chapter5/variadicFunc/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func sum(vals ...int) int {
+	/*
+	 it can take multiple arguments like we are storing it
+	 int slice of int
+	*/
+	total := 0
+	for _, val := range vals {
+		total += val
+	}
+	return total
+}
+func f(...int) {}
+func g([]int)  {}
+func main() {
+	fmt.Println(sum())           // 0
+	fmt.Println(sum(3))          // 3
+	fmt.Println(sum(1, 2, 3, 4)) // 10
+
+	value := []int{1, 2, 3, 4}
+	fmt.Println(sum(value[2], value[0]))
+
+	fmt.Printf("%T\n", f) // func(...int)
+	fmt.Printf("%T\n", g) // func([]int)
+
+	linenum, name := 12, "count"
+	errorf(linenum, "undefined: %s", name) // Line 12: undefined: count
+}
+
+func minf(vals ...int) int {
+	if len(vals) == 0 {
+		panic("minf requires at least one argument")
+	}
+	min := vals[0]
+	for _, v := range vals[1:] {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+}
+
+func maxf(vals ...int) int {
+	if len(vals) == 0 {
+		panic("maxf requare at least one argument")
+	}
+	max := vals[0]
+
+	for _, v := range vals[1:] {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func errorf(linenum int, format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, "Line %d: ", linenum)
+	fmt.Fprintf(os.Stderr, format, args...)
+	fmt.Fprintln(os.Stderr)
+}
