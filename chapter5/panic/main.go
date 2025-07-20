218f89f58f6667b398556e5998c25a831c422bfe
Aniket Mahajan <mahajananiket207@gmail.com>
2025-07-20 09:51:48 +0530

    panic

--- chapter5/panic/main.go
+++ chapter5/panic/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 )
 
 func main() {
+	defer printStack()
 	f(3)
 }
 
@@ -12,4 +15,52 @@ func f(x int) {
 	fmt.Printf("f(%d)\n", x+0/x) // panic if x == 0
 	defer fmt.Printf("defer %d\n", x)
 	f(x - 1)
+}
+
+func printStack() {
+	var buf [4096]byte
+	n := runtime.Stack(buf[:], false)
+	os.Stdout.Write(buf[:n])
+}
+
+func Parse(input string) (s *Syntax, err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			err = fmt.Errorf("internal error: %v", p)
+		}
+	}()
+	// ...parser...
+}
+
+// doleTitle returns the text of the first non-empty title element
+// in doc, and an error if there was not exactly one.
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+			//no panic
+		case bailout{}:
+			// expencted panic
+			err = fmt.Errorf("multiple title elements")
+		default:
+			panic(p) // unexpected panic; carry on panicking
+		}
+	}()
+
+	// Bai out of recursion if we find more than non-empty title.
+	forEachNode(doc, func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" &&
+			n.FirstChild != nil {
+			if title != "" {
+				panic(bailout{}) // multiple title elements
+			}
+			title = n.FirstChild.Data
+		}
+	}, nil)
+	if title == "" {
+		return "", fmt.Errorf("no title element")
+	}
+	return title, nil
 }
